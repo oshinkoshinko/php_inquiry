7713fca51763181903b554af8b38f3ff49410458
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-05-22 14:32:07 +0900

    Add pageFlag for switching screen

--- form/input.php
+++ form/input.php
@@ -5,6 +5,15 @@ if(!empty($_POST['your_name'])){
   var_dump($_POST);
   echo '</pre>';
 }
+$pageFlag = 0;
+
+if(!empty($_POST['btn_confirm'])){
+  $pageFlag = 1;
+}
+
+if(!empty($_POST['btn_submit'])){
+  $pageFlag = 2;
+}
 
 ?>
 
@@ -13,15 +22,38 @@ if(!empty($_POST['your_name'])){
 <head></head>
 <body>
 
+
+
+<?php if($pageFlag === 1): ?>
+<form method="POST" action="input.php">
+氏名
+<?php echo $_POST['your_name'] ;?>
+<br>
+メールアドレス
+<?php echo $_POST['email'] ;?>
+<br>
+<input type="submit" name="btn_submit" value="送信する">
+<input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
+<input type="hidden" name="email" value="<?php echo $_POST['email'] ;?>">
+</form>
+<?php endif; ?>
+
+<?php if($pageFlag === 2 ): ?>
+完了画面
+<?php endif; ?>
+
+<?php if($pageFlag === 0 ): ?>
+
 <form method="POST" action="input.php">
 氏名
 <input type="text" name="your_name">
 <br>
-<input type="checkbox" name="sports[]" value="baseball">野球
-<input type="checkbox" name="sports[]" value="soccer">サッカー
-<input type="checkbox" name="sports[]" value="tennis">テニス
+メールアドレス
+<input type="email" name="email">
+<br>
+<input type="submit" name="btn_confirm" value="確認する">
+</form>
 
-<input type="submit" value="送信">
-
+<?php endif; ?>
 </body>
 </html>
