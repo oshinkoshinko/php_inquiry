651bdf5b634da00eaed595f38513813cdc50a91d
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-05-25 16:33:55 +0900

    Add file controll

--- mainte/test.php
+++ mainte/test.php
@@ -6,5 +6,27 @@ echo __FILE__;
 echo '<br>';
 //パスワード暗号化
 echo password_hash('password123', PASSWORD_BCRYPT);
+echo '<br>';
+
+$contactFile = '.contact.dat';
+// $fileContents = file_get_contents($contactFile);
+// echo $fileContents
+
+//書き込む
+// file_put_contents($contactFile, 'テストです')
+
+// $addText = 'テストです' . "\n";
+// //追記
+// file_put_contents($contactFile, $addText, FILE_APPEND);
+
+//fileで配列へ explodeで,区切り foreachで抽出
+
+$allData = file($contactFile);
+foreach($allData as $lineData){
+    $lines = explode(',', $lineData);
+    echo $lines[0]. '<br>';
+    echo $lines[1]. '<br>';
+    echo $lines[2]. '<br>';
+}
 
 ?>
